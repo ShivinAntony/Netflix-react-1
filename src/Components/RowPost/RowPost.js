25d709ac3f133b8fe7f6a4ab1945476609a22f74
Shivin Antony <shivinantony087@gmail.com>
2024-04-17 11:00:56 +0530

    movie rowlist added

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,25 +1,25 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from '../../axios'
-import {API_KEY,imageUrl} from '../../constants/constants'
+import { imageUrl } from '../../constants/constants'
 import './RowPost.css';
-function RowPost() {
+function RowPost(props) {
   const [movies, setMovies] = useState([])
-  useEffect(()=>{
-    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response=>{
+  useEffect(() => {
+    axios.get(props.url).then(response => {
       console.log(response.data)
       setMovies(response.data.results)
-    }).catch(err=>{
+    }).catch(err => {
       //alert('Network error')
     })
-},[])
+  }, [])
   return (
     <div className='row'>
-      <h2>Netflix Originals</h2>
+      <h2>{props.title} </h2>
       <div className='posters'>
-        {movies.map((obj)=>
-      <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-    )}
-      
+        {movies.map((obj) =>
+          <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
+        )}
+
       </div>
     </div>
   )
